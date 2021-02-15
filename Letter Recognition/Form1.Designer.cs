9501e3b45838fed8d8f313641f118dc36f0a7bb5
Folen0909 <davidturkalj@gmail.com>
2021-02-15 19:42:34 +0100

    GUI added

--- Letter Recognition/Form1.Designer.cs
+++ Letter Recognition/Form1.Designer.cs
@@ -29,13 +29,95 @@ namespace Letter_Recognition
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.resultBtn = new System.Windows.Forms.Button();
+            this.PredictionLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(560, 560);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(578, 12);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.clearBtn.Size = new System.Drawing.Size(100, 50);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = false;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.submitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(578, 68);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.submitBtn.Size = new System.Drawing.Size(100, 50);
+            this.submitBtn.TabIndex = 2;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            // 
+            // resultBtn
+            // 
+            this.resultBtn.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.resultBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBtn.Location = new System.Drawing.Point(578, 124);
+            this.resultBtn.Name = "resultBtn";
+            this.resultBtn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.resultBtn.Size = new System.Drawing.Size(100, 50);
+            this.resultBtn.TabIndex = 3;
+            this.resultBtn.Text = "Result";
+            this.resultBtn.UseVisualStyleBackColor = false;
+            // 
+            // PredictionLabel
+            // 
+            this.PredictionLabel.AutoSize = true;
+            this.PredictionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 400F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PredictionLabel.Location = new System.Drawing.Point(685, 13);
+            this.PredictionLabel.Name = "PredictionLabel";
+            this.PredictionLabel.Size = new System.Drawing.Size(430, 604);
+            this.PredictionLabel.TabIndex = 4;
+            this.PredictionLabel.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1310, 704);
+            this.Controls.Add(this.PredictionLabel);
+            this.Controls.Add(this.resultBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button resultBtn;
+        private System.Windows.Forms.Label PredictionLabel;
     }
 }
 
